988554ba24ff1c70d4b1910d8a8af538f56e2618
ceefour <ceefour@7470731c-3a11-4522-bcce-b5c5502cf0d2>
2007-11-08 13:21:13 +0000

    Cleanup commands.
    git-svn-id: http://subversion.assembla.com/svn/ccdd/creator@7 7470731c-3a11-4522-bcce-b5c5502cf0d2

--- trunk/src/rakefile.rb
+++ trunk/src/rakefile.rb
@@ -13,17 +13,12 @@ namespace :svn do
     ['NAME', 'HOST', 'USER'].each do |env_name|
       raise "Requires #{env_name}" unless ENV[env_name]
     end
-    [Escape.shell_command(["cap", "invoke", "SUDO=1",
-          "HOSTS=#{ENV['HOST']}",
-          "COMMAND="+ Escape.shell_command(["svnadmin", "create", "/media/data/server/svn/#{ENV['NAME']}"])
-        ]),
-      Escape.shell_command(["cap", "invoke", "SUDO=1",
-          "HOSTS=#{ENV['HOST']}",
-          "COMMAND="+ Escape.shell_command(['chown', '-R', 'www-data:www-data', "/media/data/server/svn/#{ENV['NAME']}"])
-        ]) ].each do |cmd|
-      $stderr.puts "Executing: #{cmd}..."
-      system cmd
-    end
+    svnadmin_cmd = Escape.shell_command(["svnadmin", "create", "/media/data/server/svn/#{ENV['NAME']}"])
+    sh Escape.shell_command(["cap", "invoke", "SUDO=1", "HOSTS=#{ENV['HOST']}",
+        "COMMAND=#{svnadmin_cmd}"])
+    chown_cmd = Escape.shell_command(['chown', '-R', 'www-data:www-data', "/media/data/server/svn/#{ENV['NAME']}"])
+    sh Escape.shell_command(["cap", "invoke", "SUDO=1", "HOSTS=#{ENV['HOST']}",
+        "COMMAND=#{chown_cmd}"])
   end
 
   # Params: NAME, HOST (svn server)
@@ -33,10 +28,7 @@ namespace :svn do
     ['NAME', 'HOST'].each do |env_name|
       raise "Requires #{env_name}" unless ENV[env_name]
     end
-    [Escape.shell_command(["svn", "info", "http://#{ENV['HOST']}/#{ENV['NAME']}"])].each do |cmd|
-      $stderr.puts "Executing: #{cmd}..."
-      system cmd
-    end
+    sh Escape.shell_command(["svn", "info", "http://#{ENV['HOST']}/#{ENV['NAME']}"])
   end
 
   # Params: NAME, HOST (svn server)
@@ -46,10 +38,7 @@ namespace :svn do
     ['NAME', 'HOST'].each do |env_name|
       raise "Requires #{env_name}" unless ENV[env_name]
     end
-    [Escape.shell_command(["svn", "ls", "-v", "http://#{ENV['HOST']}/#{ENV['NAME']}"])].each do |cmd|
-      $stderr.puts "Executing: #{cmd}..."
-      system cmd
-    end
+    sh Escape.shell_command(["svn", "ls", "-v", "http://#{ENV['HOST']}/#{ENV['NAME']}"])
   end
 
   namespace :module do
@@ -60,18 +49,14 @@ namespace :svn do
       ['URL', 'MODULE'].each do |env_name|
         raise "Requires #{env_name}" unless ENV[env_name]
       end
-      [Escape.shell_command(["svn", "mkdir",
-            "#{ENV['URL']}/#{ENV['MODULE']}", '-m', "Created top-level folder for module project '#{ENV['MODULE']}'"]),
-        Escape.shell_command(["svn", "mkdir",
-            "#{ENV['URL']}/#{ENV['MODULE']}/trunk", '-m', "Created trunk folder for module project '#{ENV['MODULE']}'"]),
-        Escape.shell_command(["svn", "mkdir",
-            "#{ENV['URL']}/#{ENV['MODULE']}/tags", '-m', "Created tags folder for module project '#{ENV['MODULE']}'"]),
-        Escape.shell_command(["svn", "mkdir",
-            "#{ENV['URL']}/#{ENV['MODULE']}/branches", '-m', "Created branches folder for module project '#{ENV['MODULE']}'"])
-      ].each do |cmd|
-        $stderr.puts "Executing: #{cmd}..."
-        system cmd
-      end
+      sh Escape.shell_command(["svn", "mkdir",
+          "#{ENV['URL']}/#{ENV['MODULE']}", '-m', "Created top-level folder for module project '#{ENV['MODULE']}'"])
+      sh Escape.shell_command(["svn", "mkdir",
+          "#{ENV['URL']}/#{ENV['MODULE']}/trunk", '-m', "Created trunk folder for module project '#{ENV['MODULE']}'"])
+      sh Escape.shell_command(["svn", "mkdir",
+          "#{ENV['URL']}/#{ENV['MODULE']}/tags", '-m', "Created tags folder for module project '#{ENV['MODULE']}'"])
+      sh Escape.shell_command(["svn", "mkdir",
+          "#{ENV['URL']}/#{ENV['MODULE']}/branches", '-m', "Created branches folder for module project '#{ENV['MODULE']}'"])
     end
   end
   
@@ -90,6 +75,22 @@ namespace :svn do
       $stderr.puts "Executing: #{cmd}..."
       system cmd
     end
+  end
+  
+  desc 'Provides soft importing of projects.'
+  namespace :import do
+    
+    # Params: PATH, URL (will be imported at THIS folder, it won't create a subfolder)
+    #   OUTPUT (project will be checked out here, left uncommitted)
+    desc 'Imports a Rails project as a new checkout, not yet committed.'
+    task :rails do
+      require 'escape'
+      %w{ PATH URL OUTPUT }.each do |env_name|
+        raise "Requires #{env_name}" unless ENV[env_name]
+      end
+      sh Escape.shell_command(["svn", "checkout", ENV['URL'], ENV['OUTPUT']])
+    end
+    
   end
   
 end
